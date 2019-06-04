09ba0eef66b07ff6134f88c4ea092c7f9c7f38fc
Sanel Hodzic <sanel_hodzic_tz_91@hotmail.com>
2019-06-04 15:02:46 +0200

    LeaderBoard

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Character.create([
+   {name: "Wally", positionX: 646, positionY: 2262},
+   {name: "Wenda", positionX: 2472, positionY: 2719},
+   {name: "Wizard Whitebeard", positionX: 3427, positionY: 2637},
+   {name: "Woof", positionX: 91, positionY: 2520},
+   {name: "Odlaw", positionX: 2102, positionY: 3072}
+])
